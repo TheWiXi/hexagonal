430f467c2d40d4fc005b2bd265c30a6ee5b3dc20
TheWiXi <87704776+TheWiXi@users.noreply.github.com>
2024-09-20 10:15:46 -0500

    feat: :sparkles: se añade configuracion del servidor

---
+++ infrastructure/server/server.js
@@ -0,0 +1,32 @@
+// Configuración y puesta en marcha del servidor Express.
+const express = require('express');
+const passport = require('passport');
+const sessionGoogleOAuth = require('../middlewares/sessionOAuth');
+const path = require('path');
+const indexRouter = require('../../application/routes/indexRouter');
+const loginRouter = require('../../application/routes/loginRouter');
+const createAccountRouter = require('../../application/routes/createAccountRouter');
+const userRoutes = require('../../application/routes/userRoutes');
+const productRoutes = require('../../application/routes/userProductosRouter');
+const { jsonParseErrorHandler } = require('../middlewares/errorHandling');
+const { limiTotal } = require('../middlewares/rateLimit');
+
+
+const createServer = () => {
+    const app = express();
+    
+    app.use(jsonParseErrorHandler);    
+    app.use(limiTotal);
+    app.use('/css', express.static(path.join(process.env.EXPRESS_STATIC, 'css')));
+    app.use('/js', express.static(path.join(process.env.EXPRESS_STATIC, 'js')));
+    app.use('/storage', express.static(path.join(process.env.EXPRESS_STATIC, 'storage')));
+  
+    app.use('/', indexRouter);
+    app.use('/login', sessionGoogleOAuth, passport.initialize(), passport.session(), loginRouter);
+    app.use('/createAccount', createAccountRouter);
+    app.use('/users', userRoutes);
+    app.use('/home', productRoutes);
+    return app;
+};
+
+module.exports = createServer;
